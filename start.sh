c0ca292b1259250819710c332e24c23d5e6d3b68
Suryansh vikram singh <69345576+suryanshvikramsingh@users.noreply.github.com>
2023-04-15 00:26:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /autofiltershortnerbot
 fi
-cd /ben-url-filter-bot
+cd /autofiltershortnerbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
